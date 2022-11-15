d8117e860041b8837cc2f7f04798c0e570a4bda3
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-11-15 19:25:20 -0300

    Actualiza mecanica juego

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const estado = 'presione-boton';
+let estado = 'click';
 let patron = [];
 let nivel = 0;
+let patronIndiceJugador = 0;
 
 const tituloComienzo = document.querySelector('#comienzo');
 const botonRojo = document.querySelector('#rojo');
@@ -10,29 +11,58 @@ const botonAzul = document.querySelector('#azul');
 
 let boton = [botonRojo, botonVerde, botonAmarillo, botonAzul];
 
-document.addEventListener('click', comenzarJuego);
+document.querySelector('#play').onclick = function () {
+    comenzarJuego();
+};
+botonRojo.addEventListener('click', presionarBoton);
+botonVerde.addEventListener('click', presionarBoton);
+botonAmarillo.addEventListener('click', presionarBoton);
+botonAzul.addEventListener('click', presionarBoton);
 
 function comenzarJuego() {
-    if (estado === 'presione-boton') {
+    if (estado === 'click' || estado === 'Game Over') {
         nuevoNivel();
+        patron = [];
+        nivel = 0;
+        patronIndiceJugador = 0;
     }
 }
 
 function nuevoNivel() {
+    estado = 'click';
     setTimeout(() => {
-        nivel = nivel + 1;
+        nivel += 1;
         tituloComienzo.innerText = `Nivel  ${nivel}`;
         let proximoColor = Math.floor(Math.random() * 4);
         let proximoBoton = boton[proximoColor];
 
         botonClaro(proximoBoton);
         patron.push(proximoBoton);
+
+        patronIndiceJugador = 0;
+
+        estado = 'esperando-jugador';
     }, 500);
 }
 
 function botonClaro(boton) {
-    botonClaro.classList.add('active');
+    boton.classList.toggle('active');
     setTimeout(() => {
-        botonClaro.classList.remove('active');
+        boton.classList.remove('active');
     }, 2000);
+}
+
+function presionarBoton(event) {
+    if (estado === 'esperando-jugador') {
+        const boton = event.target;
+        if (boton === patron[patronIndiceJugador]) {
+            patronIndiceJugador = patronIndiceJugador + 1;
+            botonClaro(boton);
+
+            nuevoNivel();
+        } else {
+            estado = 'Game Over';
+            tituloComienzo.innerText = 'Game Over';
+        }
+    }
 }
