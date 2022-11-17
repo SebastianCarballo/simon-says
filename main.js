1f890e86938303135fc00fdca59fbee2a7aa29dd
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-11-16 21:23:05 -0300

    Actualiza mecánica juego simon dice

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const botonAzul = document.querySelector('#azul');
 let boton = [botonRojo, botonVerde, botonAmarillo, botonAzul];
 
 document.querySelector('#play').onclick = function () {
-    comenzarJuego();
+	comenzarJuego();
 };
 botonRojo.addEventListener('click', presionarBoton);
 botonVerde.addEventListener('click', presionarBoton);
@@ -20,49 +20,47 @@ botonAmarillo.addEventListener('click', presionarBoton);
 botonAzul.addEventListener('click', presionarBoton);
 
 function comenzarJuego() {
-    if (estado === 'click' || estado === 'Game Over') {
-        nuevoNivel();
-        patron = [];
-        nivel = 0;
-        patronIndiceJugador = 0;
-    }
+	if (estado === 'click' || estado === 'Game Over') {
+		nuevoNivel();
+		patron = [];
+		nivel = 0;
+		patronIndiceJugador = 0;
+	}
 }
 
 function nuevoNivel() {
-    estado = 'click';
-    setTimeout(() => {
-        nivel += 1;
-        tituloComienzo.innerText = `Nivel  ${nivel}`;
-        let proximoColor = Math.floor(Math.random() * 4);
-        let proximoBoton = boton[proximoColor];
-
-        botonClaro(proximoBoton);
-        patron.push(proximoBoton);
-
-        patronIndiceJugador = 0;
-
-        estado = 'esperando-jugador';
-    }, 500);
+	estado = 'esperado-patron';
+	setTimeout(() => {
+		nivel += 1;
+		tituloComienzo.innerText = `Nivel  ${nivel}`;
+		const proximoColor = Math.floor(Math.random() * 4);
+		const proximoBoton = boton[proximoColor];
+		iluminarBoton(proximoBoton);
+		patron.push(proximoBoton);
+		patronIndiceJugador = 0;
+		estado = 'esperando-jugador';
+	}, 500);
 }
 
-function botonClaro(boton) {
-    boton.classList.toggle('active');
-    setTimeout(() => {
-        boton.classList.remove('active');
-    }, 2000);
+function iluminarBoton(boton) {
+	boton.classList.toggle('active');
+	setTimeout(() => {
+		boton.classList.toggle('active');
+	}, 200);
 }
 
 function presionarBoton(event) {
-    if (estado === 'esperando-jugador') {
-        const boton = event.target;
-        if (boton === patron[patronIndiceJugador]) {
-            patronIndiceJugador = patronIndiceJugador + 1;
-            botonClaro(boton);
-
-            nuevoNivel();
-        } else {
-            estado = 'Game Over';
-            tituloComienzo.innerText = 'Game Over';
-        }
-    }
+	if (estado === 'esperando-jugador') {
+		const boton = event.target;
+		if (boton === patron[patronIndiceJugador]) {
+			patronIndiceJugador = patronIndiceJugador + 1;
+			iluminarBoton(boton);
+			if(patronIndiceJugador === patron.length){
+				nuevoNivel();
+			}
+		} else {
+			estado = 'Game Over';
+			tituloComienzo.innerText = 'Game Over';
+		}
+	}
 }
